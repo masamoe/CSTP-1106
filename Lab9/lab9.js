6153bffa162c6c0dbe7e0d6aef9737b14c6ff72d
masamoe <jake_dobrinsky@hotmail.com>
2022-07-08 09:45:40 -0700

    lab9

---
+++ Lab9/lab9.js
@@ -0,0 +1,18 @@
+function process(data) {
+    $("#z").html(`Temp: ${JSON.stringify(data.main.temp)}°C Weather: ${JSON.stringify(data.weather[0].description)}`);
+};
+
+function runAjax() {
+    city = $("#x").val();
+    $.ajax({
+        url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=2e0aa7ab1b35ed151cdd308ac68f721a&units=metric`,
+        type: "get",
+        success: process,
+    });
+};
+
+function setup() {
+    $("#y").click(runAjax);
+};
+
+$(document).ready(setup);
